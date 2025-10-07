66743179047b5887a5accaad2f124947433b2fab
anagarciaaa <anagarcia1@ufl.edu>
2025-10-07 14:01:32 -0400

    Problem S1 Algorithm Implementation

--- program1.py
+++ program1.py
@@ -15,10 +15,16 @@ def program1(n: int, k: int, values: List[int]) -> Tuple[int, List[int]]:
     List[int]: the indices of the chosen vaults(1-indexed)
     """
     ############################
-    # Add you code here
-    ############################
-
-    return 0, [1, 2, 3] # replace with your code
+    #pick from the rightmost vault, then skip k to the left, repeat.
+    picks = []
+    i = n - 1 #starts at the rightmost index (but based 0 so minus 1)
+    while i >= 0:
+        picks.append(i)
+        i -= (k+1)
+    #then convert to increasing order and based 1 for output
+    picks.reverse()
+    total_value = sum(values[p] for p in picks)
+    return total_value, [p+1 for p in picks]
 
 
 if __name__ == '__main__':
